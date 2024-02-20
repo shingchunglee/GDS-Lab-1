ba9a8ce663ffd137bc0814a82ee553a34bea189e
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-02-20 13:03:27 +1100

    fix: fix movment speed

--- Assets/Scripts/input_manager.cs
+++ Assets/Scripts/input_manager.cs
@@ -8,6 +8,11 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private MovementComponent helicopterMovement;
 
+    [SerializeField]
+    public float WAIT_TIME = 0.3f;
+
+    private float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,24 +22,31 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            // TODO: Move heli up
-            helicopterMovement.MoveUp();
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            // TODO: Move heli down
-            helicopterMovement.MoveDown();
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            // TODO: Move heli left
-            helicopterMovement.MoveLeft();
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            // TODO: Move heli right
-            helicopterMovement.MoveRight();
-        }
-        if (Input.GetKeyDown(KeyCode.R)) {
-            // TODO: Reset game if possible
+        timer += Time.deltaTime;
+
+        if (timer > WAIT_TIME)
+        {
+            
+            timer = 0f;
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                // TODO: Move heli up
+                helicopterMovement.MoveUp();
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                // TODO: Move heli down
+                helicopterMovement.MoveDown();
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                // TODO: Move heli left
+                helicopterMovement.MoveLeft();
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                // TODO: Move heli right
+                helicopterMovement.MoveRight();
+            }
+            if (Input.GetKeyDown(KeyCode.R)) {
+                // TODO: Reset game if possible
+            }
         }
     }
 }
